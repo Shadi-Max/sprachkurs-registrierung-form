6e4a23f14c0d0c4118e54a65edc9d60b3df8fa80
Shadi-Max <shadimaks93@gmail.com>
2024-01-12 01:17:55 +0100

    validation error creation

--- sprachkurs-registrierung/src/App.jsx
+++ sprachkurs-registrierung/src/App.jsx
@@ -18,16 +18,19 @@ const initialValues = {
 const phoneRegExp = /^[0-9+]{9,15}$/;
 
 const validationSchema = Yup.object().shape({
-  vorname: Yup.string().required('Ihre Vorname bitte eingeben'),
-  nachname: Yup.string().required('Ihre Nachname bitte eingeben'),
+  vorname: Yup.string().required('Ihre Vorname bitte eingeben.'),
+  nachname: Yup.string().required('Ihre Nachname bitte eingeben.'),
+  birthdate: Yup.string().required('Ihre Geburtsdatum bitte eingeben.'),
+  birthplace: Yup.string().required('Ihre Geburtsort bitte eingeben.'),
+  gender: Yup.string().required('Ein Geschlecht bitte auswählen.'),
   phone: Yup.string()
-    .matches(phoneRegExp, 'Ungültige Telefonnummer')
-    .required('Ihre Telefonnummer bitte eingeben'),
-  gender: Yup.string().required('Ein Geschlecht bitte auswählen'),
-  datenschutz: Yup.string().required('Bitte die Datenschutzerklärung Zustimmen um registrieren zu können.'),
+    .matches(phoneRegExp, 'Ungültige Telefonnummer.')
+    .required('Ihre Telefonnummer bitte eingeben.'),
+  country: Yup.string().required('Ihr Herkunftsland bitte .'),
+  datenschutz: Yup.string().required('Bitte die Datenschutzerklärung Zustimmen.'),
 });
 
-const onSubmit = async (values, { setSubmitting, resetForm}) => {
+const onSubmit = async (values, {setSubmitting, resetForm}) => {
   await new Promise((r) => setTimeout(r, 500));
   alert(JSON.stringify(values, null, 2));
   console.log(values);
@@ -49,7 +52,7 @@ function App() {
               {({ isSubmitting, values, handleChange, handleBlur }) => (
                 <Form>
                   <div className="field-container" id='name'>
-                    <label id='first-name'>Name:</label>
+                    <label id='first-name'>1- Name<span className="star-symbol">*</span></label>
                     <div className="name-fields">
                       <div className="first-name">
                         <Field
@@ -74,29 +77,29 @@ function App() {
                   </div>
 
                   <div className="field-container">
-                    <label id='birth-date'>Geburtsdatum:</label>
+                    <label id='birth-date'>2- Geburtsdatum<span className="star-symbol">*</span></label>
                     <Field
                       className='field'
                       type="date"
                       name="birthdate"
                       placeholder="Geburtsdatum"
                     />
-                    <ErrorMessage name="nachname" component="div" className='error'/>
+                    <ErrorMessage name="birthdate" component="div" className='error'/>
                   </div>
 
                   <div className="field-container">
-                    <label id='birth-place'>Geburtsort:</label>
+                    <label id='birth-place'>3- Geburtsort<span className="star-symbol">*</span></label>
                     <Field
                       className='field'
                       type="text"
                       name="birthplace"
                       placeholder="Geburtsort"
                     />
-                    <ErrorMessage name="nachname" component="div" className='error'/>
+                    <ErrorMessage name="birthplace" component="div" className='error'/>
                   </div>
 
                   <div className='field-container' role="group" aria-labelledby="gender">
-                    <label id="gender">Geschlecht:</label>
+                    <label id="gender">4- Geschlecht<span className="star-symbol">*</span></label>
                     <div role="group" className='gender-group'>
                       <label id='radio'>
                         <Field type="radio" name="gender" value="man" />
@@ -114,24 +117,25 @@ function App() {
                   </div>
 
                   <div className="field-container">
-                    <label id='phone-number'>Telefonnummer:</label>
+                    <label id='phone-number'>5- Telefonnummer<span className="star-symbol">*</span></label>
                     <Field
                       className='field'
                       type="text"
                       name="phone"
-                      placeholder="Beispiel: +49 176XXXXXXX"
+                      placeholder="Beispiel: +49176XXXXXXX"
                     />
                     <ErrorMessage name="nachname" component="div" className='error'/>
                   </div>
 
                   <div className="field-container">
-                    <label id='origin-country'>Herkunftsland:</label>
+                    <label id='origin-country'>6- Herkunftsland<span className="star-symbol">*</span></label>
                     <CountryDropdown
                       className='field'
                       name='country'
                       value={values.country}
                       onChange={(_, e) => handleChange(e)} onBlur={handleBlur}
                     />
+                    <ErrorMessage name="country" component="div" className='error'/>
                   </div>
 
                   <div className='field-container'>
@@ -139,11 +143,14 @@ function App() {
                       <Field id='checkbox' type="checkbox" name="datenschutz" />
                       Ich erkläre mich weiter damit einverstanden, dass meine persönlichen Daten elektronisch gespeichert und, wenn notwendig, an Dritte weitergegeben werden.
                     </label>
+                    <ErrorMessage name="datenschutz" component="div" className='error'/>
                   </div>
 
-                  <button type="submit" disabled={isSubmitting}>
-                    Registrieren
-                  </button>
+                  <div className="button">
+                    <button type="submit" disabled={isSubmitting}>
+                      Registrieren
+                    </button>
+                  </div>
                 </Form>
               )}
             </Formik>
